51cec15ecea8906b63a2f95be72f8bc17ad6f9b0
Linus Gasser <linus.gasser@epfl.ch>
2021-05-03 11:14:42 +0200

    Add information for rust-bitcode
    The installation instructions were missing for the iOS part.
    Also some of the import parts in the xcode project were wrong.

--- ios_app/build-rust-xcode.sh
+++ ios_app/build-rust-xcode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ensure that `cargo` is in PATH, using the default location.
-export PATH="$PATH:${HOME}/.cargo/bin"
+. "$HOME/.cargo/env"
 
 set -x
 
@@ -9,7 +9,7 @@ set -x
 cd "${SRCROOT}/../"
 
 # Build binaries
-RUSTFLAGS="-Z embed-bitcode" cargo +ios-arm64 build --target aarch64-apple-ios --release --lib
+cargo +ios-arm64-1.46.0 build --target aarch64-apple-ios --release --lib
 cargo build --target=x86_64-apple-ios --release
 
 # Create fat binary
